3ad4532231d36fed1c6be7e8a45309272196118a
Michelle Maina <michellemaina510@gmail.com>
2023-08-14 17:01:38 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - The entry point
+ *
+ * Return: 0 always (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
