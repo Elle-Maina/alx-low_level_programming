56d17702a7a56b95dae6ed9abfa3658d81f4984d
Michelle Maina <michellemaina510@gmail.com>
2023-08-14 19:25:09 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - To print the alphabet.
+ *
+ * Return: 0 always (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
